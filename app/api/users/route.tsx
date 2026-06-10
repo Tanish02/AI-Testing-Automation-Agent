2bb1df25fec1569da87d84a39f04d2d2c06538d1
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2026-06-10 19:59:44 +0530

    refactor: standardize code formatting and improve consistency in user route and provider components

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,37 +1,38 @@
-import { db } from "@/db";
-import { users } from "@/db/schema";
-import { currentUser } from "@clerk/nextjs/server";
-import { eq } from "drizzle-orm";
-import { NextRequest, NextResponse } from "next/server";
+import { db } from '@/db'
+import { users } from '@/db/schema'
+import { currentUser } from '@clerk/nextjs/server'
+import { eq } from 'drizzle-orm'
+import { NextRequest, NextResponse } from 'next/server'
 
-export async function Post(req: NextRequest) {
-  const user = await currentUser();
+export async function POST(req: NextRequest) {
+  const user = await currentUser()
 
   try {
     const userResult = await db
       .select()
       .from(users)
-      .where(eq(users.email, user?.primaryEmailAddress?.emailAddress ?? ""));
+      .where(eq(users.email, user?.primaryEmailAddress?.emailAddress ?? ''))
 
-    if (userResult.length == 0) {
+    if (userResult.length === 0) {
       const newUser = await db
         .insert(users)
         .values({
-          email: user?.primaryEmailAddress?.emailAddress ?? "",
-          name: user?.fullName ?? "New User",
+          email: user?.primaryEmailAddress?.emailAddress ?? '',
+          name: user?.fullName ?? 'New User',
         })
-        .returning();
+        .returning()
 
-      return NextResponse.json({ user: newUser[0] });
-    } else {
-      return NextResponse.json({ user: userResult[0] });
+      return NextResponse.json({ user: newUser[0] })
     }
+
+    return NextResponse.json({ user: userResult[0] })
   } catch (e) {
-    console.log("Error Creating User:", e);
+    console.log('Error Creating User:', e)
+
     return NextResponse.json(
-      { error: "Failed to create new user" },
-      { status: 500 },
-    );
+      { error: 'Failed to create new user' },
+      { status: 500 }
+    )
   }
 }
 
--- app/provider.tsx
+++ app/provider.tsx
@@ -1,24 +1,24 @@
-"use client";
-import axios from "axios";
-import React, { useEffect } from "react";
+'use client'
+import axios from 'axios'
+import React, { useEffect } from 'react'
 
 function Provider({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   useEffect(() => {
-    CreateNewUser();
-  }, []);
+    CreateNewUser()
+  }, [])
   const CreateNewUser = async () => {
-    const result = await axios.post("/api/users", {});
-    console.log("Result", result);
-  };
+    const result = await axios.post('/api/users', {})
+    console.log('Result', result)
+  }
 
-  return <div>{children}</div>;
+  return <div>{children}</div>
 }
 
-export default Provider;
+export default Provider
 
 // end code
 // client side logic
