fbcc7bed4372668df3e8de3b2d4e98ff34d79aba
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2026-06-10 20:55:08 +0530

    feat: implement user detail context and update provider for user management

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,21 +1,27 @@
 'use client'
-import axios from 'axios'
-import React, { useEffect } from 'react'
 
-function Provider({
-  children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+import { UserDetailContext } from '@/context/UserDetailContext'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
+function Provider({ children }: { children: React.ReactNode }) {
+  const [userDetail, setUserDetail] = useState<any>()
+
   useEffect(() => {
     CreateNewUser()
   }, [])
+
   const CreateNewUser = async () => {
     const result = await axios.post('/api/users', {})
     console.log('Result', result)
+    setUserDetail(result.data?.user)
   }
 
-  return <div>{children}</div>
+  return (
+    <UserDetailContext.Provider value={{ userDetail, setUserDetail }}>
+      {children}
+    </UserDetailContext.Provider>
+  )
 }
 
 export default Provider
