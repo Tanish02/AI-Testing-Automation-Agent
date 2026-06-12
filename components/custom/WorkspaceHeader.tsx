9fed6524e24060f81be1760d86526fb41dcf3df9
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2026-06-12 12:32:38 +0530

    feat: enhance WorkspaceHeader layout with logo and menu options

--- components/custom/WorkspaceHeader.tsx
+++ components/custom/WorkspaceHeader.tsx
@@ -1,14 +1,21 @@
+import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
 
 const WorkspaceHeader = () => {
   return (
-    <div>
+    <div className="flex w-full justify-between p-4">
       {/* LOGO*/}
       <Image src={'/logo.svg'} alt="logo" width={200} height={400} />
 
       {/* center menu options */}
+      <ul className="flex gap-5 text-xl">
+        <li className="hover:text-blue-700 cursor-pointer">Workspace</li>
+        <li className="hover:text-blue-700 cursor-pointer">priceing</li>
+        <li className="hover:text-blue-700 cursor-pointer">Support</li>
+      </ul>
 
       {/*user button on right */}
+      <UserButton />
     </div>
   )
 }
