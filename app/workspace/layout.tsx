e9b11ae3ce96f57962fd477c70bc8472013e087f
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2026-06-12 11:57:58 +0530

    feat: add workspace layout with header component and logo

--- app/workspace/layout.tsx
+++ app/workspace/layout.tsx
@@ -1,5 +1,13 @@
+import WorkspaceHeader from '@/components/custom/WorkspaceHeader'
+import React from 'react'
+
 const workspaceLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>
+  return (
+    <div>
+      <WorkspaceHeader />
+      {children}
+    </div>
+  )
 }
 
 export default workspaceLayout
---
+++ components/custom/WorkspaceHeader.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+
+const WorkspaceHeader = () => {
+  return (
+    <div>
+      {/* LOGO*/}
+      <Image src={'/logo.svg'} alt="logo" width={200} height={400} />
+
+      {/* center menu options */}
+
+      {/*user button on right */}
+    </div>
+  )
+}
+
+export default WorkspaceHeader
