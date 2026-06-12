9a67405eb5f3030636d9e49b550635542784f446
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2026-06-12 11:16:50 +0530

    add : working on workspace layout and page

---
+++ app/workspace/layout.tsx
@@ -0,0 +1,5 @@
+const workspaceLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div>{children}</div>
+}
+
+export default workspaceLayout
---
+++ app/workspace/page.tsx
@@ -0,0 +1,5 @@
+const workspace = () => {
+  return <div>workspace</div>
+}
+
+export default workspace
