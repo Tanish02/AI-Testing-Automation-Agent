4b543f0e8e78e5805507c91d0ba1cdff748512da
Tanish Sharma <139219849+Tanish02@users.noreply.github.com>
2026-06-14 04:04:42 +0530

    Workspace Layout

--- components/custom/EmptyWorkspace.tsx
+++ components/custom/EmptyWorkspace.tsx
@@ -1,9 +1,18 @@
+import { Link } from 'lucide-react'
 import Image from 'next/image'
+import { Button } from '../ui/button'
 const EmptyWorkspace = () => {
   return (
     <div className="flex flex-col mt-10 items-center justify-center ">
       <Image src={'/folder.png'} alt="folder" width={70} height={70} />
-      <h2 className="font-medium text-3xl ">No Repository Addded</h2>
+      <h2 className="font-medium text-2xl mt-5 mb-4 ">No Repository Addded</h2>
+      <p className="text-center mx-10">
+        Connect your Github account and add a repository to get started with
+        code analysis and run test cases
+      </p>
+      <Button className="mt-5">
+        <Link className="h-5 w-4 mr-2" /> Connect Github Repo
+      </Button>
     </div>
   )
 }
